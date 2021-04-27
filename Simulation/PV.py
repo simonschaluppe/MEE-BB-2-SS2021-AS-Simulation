cfdc117f184e043f9a141efcb725528de3144530
simonschaluppe <simon.schneider@technikum-wien.at>
2021-04-27 22:29:52 +0200

    fixed PV

--- Simulation/PV.py
+++ Simulation/PV.py
@@ -8,9 +8,9 @@ Created on Mon Apr 26 13:10:17 2021
 import numpy as np
 
 
-class pv:
+class PV:
     """
-    PV Profile with .TSD in [kWh/a]
+    PV Profile with .TSD in [kWh per hour]
     """
 
     def __init__(self, csv="data/pv_1kWp.csv", kWp=1, cost_kWp=1000):
@@ -19,20 +19,30 @@ class pv:
         if csv == default_path:
             print(f"No csv-path given, loading from default {default_path}...")
             self.TSD = np.genfromtxt(csv)
+
         else:
             self.TSD = np.genfromtxt(csv)  # no specifiers, better make sure that this is right
-
+        self.path = csv
+        self.kWp = kWp
         self.cost_kWp = cost_kWp
-        self.set_kWp(kWp)
+        self.cost = kWp * self.cost_kWp
 
     def set_kWp(self, kWp):
-        self.TSD = self.TSD * kWp
+        self.TSD = self.TSD / self.kWp * kWp
         self.kWp = kWp
         self.cost = kWp * self.cost_kWp
 
+    def __repr__(self):
+        width = len(self.path)+10
+        return f"""PV-System {self.path}
+{"-"*width}
+kWp: {self.kWp:>{width-5}d}
+kWh/a: {self.TSD.sum():>{width-7}.0f}
+cost [€]: {self.cost:>{width-10}d}"""
+
 
 if __name__ == "__main__":
-    test = pv()
+    test = PV()
     import matplotlib.pyplot as plt
-
     plt.plot(test.TSD)
+    plt.show()
